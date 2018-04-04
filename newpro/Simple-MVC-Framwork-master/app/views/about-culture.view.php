60a6ff3bf2c4cd4329e00c8a32f72b1e03a58c36
Ahmed Kandil <ahmedkandil2015@hotmail.com>
2018-04-04 15:14:23 +0200

    remove simple mvc framwork

--- newpro/Simple-MVC-Framwork-master/app/views/about-culture.view.php
+++
@@ -1,7 +0,0 @@
-<?php require ('partials/header.php');?>
-
-	<h1>about culture</h1>
-	
-
-<?php require ('partials/footer.php');?>
-
--- newpro/Simple-MVC-Framwork-master/app/views/partials/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" href="/public/css/main.css">
-</head>
-<body>
-<?php require ('nav.php');?>
